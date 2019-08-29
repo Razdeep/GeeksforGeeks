6ad312fcaa2e48e186b050c69176ede36589dc9f
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2019-08-29 22:59:26 +0530

    Added Damon_prime.cpp

---
+++ Prime Numbers/Damon_prime.cpp
@@ -0,0 +1,23 @@
+// https://practice.geeksforgeeks.org/problems/damon-prime/0
+#include <iostream>
+using namespace std;
+bool isPrime(int n)
+{
+	if (n <= 1) return false;
+	for(int i = 2; i * i <= n; ++i)
+		if (n % i == 0)
+			return false;
+	return true;
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		int n;
+		cin >> n;
+		cout << (isPrime(n - 1) && isPrime(n + 1) ? "Yes" : "No") << endl;
+	}
+	return 0;
+}
